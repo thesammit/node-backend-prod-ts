fcc3950c43d5787f57e18378ce3236943a500dda
Soham Mitra <thesammit@gmail.com>
2025-03-07 01:36:10 +0530

    feat: Add prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
     languageOptions: {
@@ -16,9 +17,10 @@ export default tseslint.config({
     extends: [
         eslint.configs.recommended,
         ...tseslint.configs.recommendedTypeChecked,
+        eslintConfigPrettier,
     ],
     rules: {
         'no-console': 'error',
-        quotes: ['error', 'single', {allowTemplateLiterals: true}],
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
     },
 });
