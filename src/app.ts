2dc8a2b1cbf6b9bae2ae97a49125e1a04fc20897
Soham Mitra <thesammit@gmail.com>
2025-03-12 00:31:00 +0530

    feat: Add 404 route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express';
+import { NOT_FOUND_FN } from './constant/response-message';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import path from 'path';
 import router from './router/api-router';
 import globalErrorHandler from './middleware/global-error-handler';
+import httpError from './utils/http-error';
 
 const app: Application = express();
 
@@ -10,6 +12,17 @@ app.use(express.static(path.join(__dirname, '../', 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', router);
+
+// Not Found Route
+app.use((req: Request, _: Response, next: NextFunction) => {
+    try {
+        throw new Error(NOT_FOUND_FN(req.originalUrl));
+    } catch (err) {
+        httpError(next, { err, req, errorStatusCode: 404 });
+    }
+});
+
+// Global error handler
 app.use(globalErrorHandler);
 
 export default app;
